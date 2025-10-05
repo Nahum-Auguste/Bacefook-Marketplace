e569e194562163516592fae14311eb8fcdb4870c
Nahum Auguste <151590695+Nahum-Auguste@users.noreply.github.com>
2025-10-05 00:10:28 -0400

    Added searchbar and make a favicon logo.

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+
+
+function main() {  
+    handle_searchbar();
+    //requestAnimationFrame(main);
+}
+main();
+
+
+function handle_searchbar() {
+    /**@type {HTMLInputElement} */
+    const searchbar = document.getElementById('searchbar');
+    if (searchbar) {
+        const input = searchbar.value;
+        //console.log(input);
+        
+    }
+}
